1afbb8f6eb00a354d1877b78c5ffc0172327b425
Wechuli <wechulipaul@yahoo.com>
2019-09-19 08:42:24 +0300

    first react native introduction

---
+++ notes.md
@@ -0,0 +1,5 @@
+# React Native
+
+React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces.
+
+React primitives render to native platfrom UI, meaning your app uses the same native platform APIs other apps do. React Native lets you create truly native apps and doesn't compromise on your users' experience. It provides a core set of platform agnostic native components like `View`, `Text`, and `Image` that map directly to the platform's native UI building blocks.
---
+++ rn-first-app/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [outputText, setOutputText] = useState(
+    "Open up App.js to start working on your app!"
+  );
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        color="pink"
+        onPress={() => setOutputText("The text has changed")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
