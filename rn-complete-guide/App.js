d21e33d80061feb158fde0ac1de7ef74c48abb9c
Wechuli <wechulipaul@yahoo.com>
2019-09-21 09:24:36 +0300

    adding width

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -4,12 +4,20 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View style={{ display: "flex", flexDirection: "row" }}>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
         <TextInput
           placeholder="Course goal"
           style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 1,
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
             padding: 10
           }}
         />
