1be434c347eddf55e19d312a088fd042f5988696
Wechuli <wechulipaul@yahoo.com>
2019-09-21 09:21:49 +0300

    adding flex

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View,TextInput,Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding:30}}>
-      <View>
-      <TextInput/>
-      <Button title="ADD"/>
+    <View style={{ padding: 50 }}>
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        <TextInput
+          placeholder="Course goal"
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 10
+          }}
+        />
+        <Button title="ADD" />
       </View>
 
       <View></View>
