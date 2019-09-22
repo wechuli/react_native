a1f58a4c40cafbcdb50388d2c7f94ded562fc657
Wechuli <wechulipaul@yahoo.com>
2019-09-22 11:55:31 +0300

    improving modals

--- rn-complete-guide/components/GoalInput.js
+++ rn-complete-guide/components/GoalInput.js
@@ -3,15 +3,15 @@ import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 
 const GoalInput = props => {
   const [enteredGoal, setEnteredGoal] = useState("");
-  const { handleSubmitGoal, visible,cancelGoal } = props;
+  const { handleSubmitGoal, visible, cancelGoal } = props;
 
   const handleInputChange = enteredText => {
     setEnteredGoal(enteredText);
   };
-  const addGoalHandler = ()=>{
+  const addGoalHandler = () => {
     handleSubmitGoal(enteredGoal);
     setEnteredGoal("");
-  }
+  };
   return (
     <Modal visible={visible} animationType="slide">
       <View style={styles.inputContainer}>
@@ -21,8 +21,10 @@ const GoalInput = props => {
           onChangeText={handleInputChange}
           value={enteredGoal}
         />
-        <Button title="CANCEL" color="red" onPress={cancelGoal}/>
-        <Button title="ADD" onPress={addGoalHandler} />
+        <View style={styles.buttons}>
+          <Button title="CANCEL" color="red" onPress={cancelGoal} />
+          <Button title="ADD" onPress={addGoalHandler} />
+        </View>
       </View>
     </Modal>
   );
@@ -39,7 +41,11 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
-    marginBottom:10
+    marginBottom: 10
+  },
+  buttons: {
+    display: "flex",
+    flexDirection: "row"
   }
 });
 
