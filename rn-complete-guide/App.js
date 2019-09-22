5b56244e88b0b10397ccdd3850c73ab10f974542
Wechuli <wechulipaul@yahoo.com>
2019-09-22 09:34:01 +0300

    using flatlist for list rendering

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import uuid from "uuid/v4";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -8,8 +17,12 @@ export default function App() {
     setEnteredGoal(enteredText);
   };
   const handleSubmitGoal = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: uuid(), value: enteredGoal }
+    ]);
   };
+  console.log(uuid());
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
@@ -23,11 +36,14 @@ export default function App() {
       </View>
 
       <View>
-        {courseGoals.map(goal => (
-          <View key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
-          </View>
-        ))}
+        <FlatList
+          data={courseGoals}
+          renderItem={itemData => (
+            <View style={styles.listItem}>
+              <Text>{itemData.item.value}</Text>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -50,6 +66,7 @@ const styles = StyleSheet.create({
   },
   listItem: {
     padding: 10,
+    marginVertical: 10,
     backgroundColor: "#ccc",
     borderColor: "black",
     borderWidth: 1
