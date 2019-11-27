c25cca318dc5c79864019add33f57649c309d8db
Wechuli <wechulipaul@yahoo.com>
2019-11-27 21:59:41 +0300

    adding state

--- simpleproject/App.js
+++ simpleproject/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Paul");
+  const [person, setPerson] = useState({
+    name: "Paul",
+    age: 26
+  });
+
+  const handleButtonPress = () => {
+    setName("Wechuli");
+    setPerson({ ...person, age: person.age + 1 });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={handleButtonPress} />
+      </View>
     </View>
   );
 }
@@ -12,8 +30,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  buttonContainer: {
+    marginTop: 20
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20
+  }
 });
