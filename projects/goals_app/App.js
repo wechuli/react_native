714449610f7f2e1e291dbd437ebfa609eaf5fba1
Wechuli <wechulipaul@yahoo.com>
2020-02-04 17:42:03 +0300

    adding inline styles

--- projects/goals_app/App.js
+++ projects/goals_app/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 30 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderColor: "black",
+            padding: 10,
+            borderWidth: 1,
+            width: "70%",
+            borderRadius: 7
+          }}
+        />
+        <View style={{padding:10,height:50}}>
+          <Button title="Add" color="teal" />
+        </View>
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
