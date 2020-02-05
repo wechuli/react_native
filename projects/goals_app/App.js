aa1c41ebbaf950dd99505cf9b87d000c1e2539a6
Wechuli <wechulipaul@yahoo.com>
2020-02-05 17:40:26 +0300

    fixing conn tunnel error

--- projects/goals_app/App.js
+++ projects/goals_app/App.js
@@ -1,33 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+  d;
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
   return (
-    <View style={{ padding: 30 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center"
-        }}
-      >
+    <View style={styles.screen}>
+
+      {/* <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
-          style={{
-            borderColor: "black",
-            padding: 10,
-            borderWidth: 1,
-            width: "70%",
-            borderRadius: 7
-          }}
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <View style={{padding:10,height:50}}>
-          <Button title="Add" color="teal" />
+        <View style={{ padding: 10, height: 50 }}>
+          <Button title="Add" color="teal" onPress={addGoalHandler} />
         </View>
       </View>
-      <View></View>
+      <View></View> */}
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 30
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    borderColor: "black",
+    padding: 10,
+    borderWidth: 1,
+    width: "70%",
+    borderRadius: 7
+  }
+});
